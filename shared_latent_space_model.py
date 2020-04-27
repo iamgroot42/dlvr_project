87225e124978265538bec521b394942e404afbd2
Anshuman Suri <anshuman@email.virginia.edu>
2020-04-27 13:08:01 -0400

    Log validation metrics on all concept classifiers

--- shared_latent_space_model.py
+++ shared_latent_space_model.py
@@ -45,19 +45,19 @@ def get_cifar_dataloaders():
                                             transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
 
     trainset = torchvision.datasets.CIFAR10(root='/p/adversarialml/as9rw/datasets/cifar10', train=True, download=True, transform=transform_train)
-    trainloader = ch.utils.data.DataLoader(trainset, batch_size=200, shuffle=True, num_workers=4)
+    trainloader = ch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=4)
 
     testset = torchvision.datasets.CIFAR10(root='/p/adversarialml/as9rw/datasets/cifar10', train=False, download=True, transform=transform_validation)
-    testloader = ch.utils.data.DataLoader(testset, batch_size=200, shuffle=False, num_workers=4)
+    testloader = ch.utils.data.DataLoader(testset, batch_size=256, shuffle=False, num_workers=4)
 
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     return trainloader, testloader
 
 
 def make_small_concept_model(dim_in):
-    model = nn.Sequential(nn.Linear(dim_in, 32), nn.ReLU(True),
+    model = nn.DataParallel(nn.Sequential(nn.Linear(dim_in, 32), nn.ReLU(True),
                            nn.Linear(32, 8), nn.ReLU(True),
-                           nn.Linear(8, 1))
+                           nn.Linear(8, 1)))
     return model.cuda()
 
 
@@ -80,12 +80,11 @@ def train_shared_latent_model(model, nb_epochs, cifar_loaders, concept_loaders, 
     criterion = nn.CrossEntropyLoss()
     # Define loss for inner criterion
     criterion_concept = nn.BCEWithLogitsLoss()
-    best_model = copy.deepcopy(model.state_dict())
-
-    # Set model to training mode
-    model.train()
         
     for epoch in range(nb_epochs):
+        # Set model to training mode
+        model.train()
+
         # Each epoch has a training and validation phase
         print("Epoch : %d" % (epoch + 1))
         examples_so_far = 0
@@ -156,9 +155,10 @@ def train_shared_latent_model(model, nb_epochs, cifar_loaders, concept_loaders, 
                 optimizer.step()
 
             # Update logging stats
-            iterator.set_description('Aux Loss: %.3f Loss: %.3f Accuracy: %.3f' % (logs['Aux_Loss'] / examples_so_far_inside,
+            iterator.set_description('Aux Loss: %.2f Loss: %.2f Accuracy: %.2f' % (logs['Aux_Loss'] / examples_so_far_inside,
                                                                                    logs['Loss'] / examples_so_far,
                                                                                    100 * logs['Accuracy'] / examples_so_far))
+
             
         # Normalize and write the data to TensorBoard
         logs['Loss']      /= examples_so_far
@@ -167,15 +167,66 @@ def train_shared_latent_model(model, nb_epochs, cifar_loaders, concept_loaders, 
         writer.add_scalars('Loss', {"train": logs['Loss']}, epoch)
         writer.add_scalars('Accuracy', {"train": logs['Accuracy']}, epoch)
             
-        print("%s loss: %.3f , accuracy: %.3f" % ("train", logs['Loss'], logs['Accuracy']))
+        # Log statistics on train data
+        print("%s loss: %.2f , accuracy: %.2f" % ("train", logs['Loss'], 100 * logs['Accuracy']))
+
+        # Calculate accuracy metrics on CIFAR-10 validation
+        model.eval()
+        acc, count = 0, 0
+        for im, label in cifar_loaders[1]:
+            im, label = im.cuda(), label.cuda()
+            with ch.no_grad():
+                prediction = model(im)
+            acc += ch.sum(ch.argmax(prediction, 1) == label.data).item()
+            count += im.shape[0]
+        cifar10_val_acc = acc / count
+
+        # Calculate accuracy statistics on all other data loaders
+        concept_accs = [[0 for j in range(m)] for m in mappings]
+        for i in tqdm(range(len(concept_loaders))):
+            concept_count = 0
+            # Across all classes
+            for im, label in concept_loaders[i][1]:
+
+                im, label = im.cuda(), label.cuda()
+                with ch.no_grad():
+                    latent, _ = model(im, with_latent=True)
+                concept_count += im.shape[0]
+
+                # Across all concepts
+                for j in range(mappings[i]):
+                    label_ = []
+                    # Convert labels using the given mapping
+                    for k, l in enumerate(label):
+                        label_.append((l.item() == j) * 1)
+                    label_ = np.array(label_)
+
+                    # Filter out relevant latent code
+                    latent_code = latent[:, latent_ranges[i][j][0]: latent_ranges[i][j][1]]
+
+                    # Pass through corresponding concept classifier
+                    with ch.no_grad():
+                        prediction = concept_models[i][j](latent_code)
+                    concept_accs[i][j] += np.sum((ch.sigmoid(prediction) >= 0.5).cpu().numpy().squeeze(-1) == label_)
+
+            # Normalize to obtain accuracies
+            for j in range(mappings[i]):
+                concept_accs[i][j] /= concept_count
+
+        # Flatten out concept accuracies to single list
+        concept_accs_flat = []
+        for x in concept_accs:
+            for y in x:
+                concept_accs_flat.append(100 *y)
+        # Compute mean, std of concept classifier accuracies
+        concept_mean, concept_std = np.mean(concept_accs_flat), np.std(concept_accs_flat)
+
+        # Log statistics on validation data
+        print("%s cifar10-acc: %.2f , concepts-acc mean: %.2f , concepts-acc std : %.2f" % ("val", 100 *cifar10_val_acc, concept_mean, concept_std))
                  
-    #     # Write best weights to disk
-    #     if epoch % 5 == 0 or epoch == nb_epochs - 1:
-    #         ch.save(best_model, "./shared_latent_space_model.pt")
-    
-    # final_accuracy = test_model(model, params)
-    # writer.add_text('Final_Accuracy', str(final_accuracy), 0)
-    # writer.close()
+        # Write best weights to disk
+        if epoch % 5 == 0 or epoch == nb_epochs - 1:
+            ch.save(model, "./shared_latent_space_model.pt")
 
 
 if __name__ =="__main__":
@@ -208,7 +259,7 @@ if __name__ =="__main__":
     # Creaate normal CIFAR-10 loader
     cifar_loaders = get_cifar_dataloaders()
     # Define model
-    model = nn.DataParallel(vgg_model.vgg19_bn(pretrained=False, num_latent=per_class_concept_latent * num_total_concepts))
+    model = nn.DataParallel(vgg_model.vgg19_bn(pretrained=False, num_latent=per_class_concept_latent * num_total_concepts)).cuda()
     #  Train model with shared encoders
     nb_epochs = 100
     train_shared_latent_model(model, nb_epochs,
